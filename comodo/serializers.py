3131643d4bf8e7e356e82c5ab434e058df29199d
dyrnade <gurescicem@gmail.com>
2016-03-24 19:56:54 +0200

    api fix

--- comodo/serializers.py
+++ comodo/serializers.py
@@ -1,34 +1,9 @@
 from rest_framework import serializers
 from comodo.models import Post
 
-
-# class PostSerializer(serializers.Serializer):
-#     pk = serializers.IntegerField(read_only=True)
-#     title = serializers.CharField(required=False, allow_blank=True, max_length=120)
-#     # message = serializers.CharField(style={'base_template': 'textarea.html'})
-#     message = serializers.CharField()
-#     is_accomplished = serializers.BooleanField(required=False,default=False)
-#
-#     def create(self, validated_data):
-#         """
-#         Create and return a new `Snippet` instance, given the validated data.
-#         """
-#         return Post.objects.create(**validated_data)
-#
-#     def update(self, instance, validated_data):
-#         """
-#         Update and return an existing `Snippet` instance, given the validated data.
-#         """
-#         instance.title = validated_data.get('title', instance.title)
-#         instance.code = validated_data.get('code', instance.code)
-#         instance.linenos = validated_data.get('linenos', instance.linenos)
-#         instance.language = validated_data.get('language', instance.language)
-#         instance.style = validated_data.get('style', instance.style)
-#         instance.save()
-#         return instance
-
 class PostSerializer(serializers.ModelSerializer):
-    # tracks = serializers.StringRelatedField(many=True)
     class Meta:
         model = Post
-        fields = ('user','title', 'message', 'is_accomplished')
+        fields = ('id','title', 'message', 'is_accomplished')
+        # filter_backends = (filters.DjangoFilterBackend,)
+        # filter_fields = ('username',)
